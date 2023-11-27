728f09ab2ea936617dedb4023bb6ff25009f5843
김승우 <tmd8633@naver.com>
2023-11-27 15:58:05 +0900

    Add : BCryptPasswordEncoder add

--- src/main/java/football/start/allOfFootball/common/BCrypt.java
+++ src/main/java/football/start/allOfFootball/common/BCrypt.java
@@ -4,21 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
 public class BCrypt {
 
 
-    private final BCryptPasswordEncoder bc;
-    @Autowired
-    public BCrypt(BCryptPasswordEncoder bc) {
-        this.bc = bc;
-    }
+    private static final BCryptPasswordEncoder bc = new BCryptPasswordEncoder(10);
 
-    public String encodeBCrypt(String password) {
+    public static String encodeBCrypt(String password) {
         return bc.encode(password);
     }
 
-    public boolean matchBCrypt(String password, String encodedPassword) {
+    public static boolean matchBCrypt(String password, String encodedPassword) {
         return bc.matches(password, encodedPassword);
     }
+
 }
