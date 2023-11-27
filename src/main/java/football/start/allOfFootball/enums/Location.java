d270b1d1363a99185a86f3971758dbb533381880
김승우 <tmd8633@naver.com>
2023-11-27 16:10:35 +0900

    Feat : Location enum 추가

---
+++ src/main/java/football/start/allOfFootball/enums/Location.java
@@ -0,0 +1,22 @@
+package football.start.allOfFootball.enums;
+
+public enum Location {
+
+    서울,
+    경기,
+    인천,
+    강원,
+    대전,
+    세종,
+    충남,
+    충북,
+    대구,
+    경북,
+    경남,
+    부산,
+    울산,
+    광주,
+    전북,
+    전남,
+    제주;
+}
