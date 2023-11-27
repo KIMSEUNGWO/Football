b6b354defa10b3ff5627610705a7813f33436dfe
김승우 <tmd8633@naver.com>
2023-11-27 16:05:19 +0900

    Add : fetch js, 공통 css 추가

---
+++ src/main/resources/static/js/fetch.js
@@ -0,0 +1,15 @@
+function fetchPost(url, json, callback) {
+    fetch(url , { method : 'post'
+					, headers : {'Content-Type' : 'application/json'}
+					, body : JSON.stringify(json)
+				})
+    .then(res => res.json())
+    .then(map => callback(map));
+}
+
+function fetchGet(url, callback) {
+    fetch(url)
+    .then(res => res.json())
+    .then(map => callback(map));
+}
+
