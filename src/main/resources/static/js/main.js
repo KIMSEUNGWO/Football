d05a995af6f76812a182dc38633de8f4405aa46e
김승우 <tmd8633@naver.com>
2023-11-27 16:04:21 +0900

    Add : 메인페이지 추가

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,39 @@
+window.addEventListener('load', function(){
+    var region = document.querySelector('.region');
+    var gender = document.querySelector('.gender');
+    var genderOption = document.querySelector('.genderOption');
+    var regionOption = document.querySelector('.regionOption');
+
+    region.addEventListener('click', function(e){
+        if (e.target.isEqualNode(region)) {
+            var regionOption = document.querySelector('.regionOption');
+            addDisabled(regionOption);
+            regionOption.classList.toggle('disabled');
+        }
+    })
+    gender.addEventListener('click', function(e){
+        if (e.target.isEqualNode(gender)) {
+            addDisabled(genderOption);
+            genderOption.classList.toggle('disabled');
+        }
+    })
+
+    this.document.addEventListener('mouseup', function(e){
+        if (!region.contains(e.target)) {
+            regionOption.classList.add('disabled');
+        }
+        if (!gender.contains(e.target)) {
+            genderOption.classList.add('disabled');
+        }
+
+    })
+})
+
+function addDisabled(e) {
+    var optionList = document.querySelectorAll('.subOption');
+    for (let i=0;i<optionList.length;i++){
+        if (!optionList[i].isEqualNode(e)) {
+            optionList[i].classList.add('disabled');
+        }
+    }
+}
