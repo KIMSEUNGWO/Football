da65885866d52074f5a7c343e94b77f41054c386
김승우 <tmd8633@naver.com>
2023-11-27 16:10:06 +0900

    Feat : 세션 상수 추가

--- src/main/java/football/start/allOfFootball/SessionConst.java
+++ src/main/java/football/start/allOfFootball/SessionConst.java
@@ -3,5 +3,6 @@ package football.start.allOfFootball;
 public abstract class SessionConst {
 
     public static final String LOGIN_MEMBER = "memberId";
+    public static final String REGISTER = "registerId";
     public static final String REDIRECT_URL = "redirectURL";
 }
