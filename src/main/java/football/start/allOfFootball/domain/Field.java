016e30de82c704868b3ff4161f0a6f636d343ea9
김승우 <tmd8633@naver.com>
2023-11-28 20:08:40 +0900

    Feat : Field Builder 기능 추가

--- src/main/java/football/start/allOfFootball/domain/Field.java
+++ src/main/java/football/start/allOfFootball/domain/Field.java
@@ -1,15 +1,20 @@
 package football.start.allOfFootball.domain;
 
-import football.start.allOfFootball.enums.Location;
+import football.start.allOfFootball.controller.admin.SaveGroundForm;
+import football.start.allOfFootball.enums.LocationEnum;
+import football.start.allOfFootball.enums.groundEnums.ParkingEnum;
+import football.start.allOfFootball.enums.groundEnums.ShowerEnum;
+import football.start.allOfFootball.enums.groundEnums.ToiletEnum;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @ToString
 @Entity
+@Builder
 @Table(name = "FIELD")
 @SequenceGenerator(name = "SEQ_FIELD", sequenceName = "SEQ_FIELD_ID", allocationSize = 1)
 public class Field {
@@ -17,14 +22,33 @@ public class Field {
     @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_FIELD")
     private Long fieldId;
     private String fieldTitle;
+    @Enumerated(EnumType.STRING)
+    private LocationEnum fieldLocation;
     private String fieldAddress;
 
-    @Enumerated(EnumType.STRING)
-    private Location fieldLocation1; // enum으로 대체 예정
-    private String fieldLocation2;
+    @Lob
+    @Column(columnDefinition = "CLOB")
     private String fieldInformation;
-    private char fieldParkingStatus;
-    private char fieldShowerStatus;
-    private int fieldMinPerson;
-    private int fieldMaxPerson;
+
+    @Enumerated(EnumType.STRING)
+    private ParkingEnum fieldParking;
+
+    @Enumerated(EnumType.STRING)
+    private ToiletEnum fieldToilet;
+
+    @Enumerated(EnumType.STRING)
+    private ShowerEnum fieldShower;
+
+    public static Field build(SaveGroundForm form) {
+        return new Field().builder()
+            .fieldTitle(form.getFieldName())
+            .fieldLocation(form.getRegion())
+            .fieldAddress(form.getFieldAddress())
+            .fieldParking(form.getParking())
+            .fieldToilet(form.getToilet())
+            .fieldShower(form.getShower())
+            .fieldInformation(form.getFieldDetails())
+            .build();
+    }
+
 }
