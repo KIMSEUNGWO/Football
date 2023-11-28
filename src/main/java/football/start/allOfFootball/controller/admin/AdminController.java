57bc620488d3b5d70261ef812eab916ce1a8a07d
김승우 <tmd8633@naver.com>
2023-11-28 21:38:37 +0900

    Feat : 지역 Enum -> Thymeleaf로 확장성 증가

--- src/main/java/football/start/allOfFootball/controller/admin/AdminController.java
+++ src/main/java/football/start/allOfFootball/controller/admin/AdminController.java
@@ -1,9 +1,11 @@
 package football.start.allOfFootball.controller.admin;
 
+import football.start.allOfFootball.enums.LocationEnum;
 import football.start.allOfFootball.service.AdminService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,12 +23,16 @@ public class AdminController {
     private final AdminService adminService;
 
     @GetMapping("/ground")
-    public String ground() {
+    public String ground(Model model) {
+        LocationEnum[] locations = LocationEnum.values();
+        model.addAttribute("locations", locations);
         return "admin_ground";
     }
 
     @GetMapping("/ground/add")
-    public String groundAdd(@ModelAttribute SaveGroundForm saveGroundForm) {
+    public String groundAdd(@ModelAttribute SaveGroundForm saveGroundForm, Model model) {
+        LocationEnum[] locations = LocationEnum.values();
+        model.addAttribute("locations", locations);
         return "admin_ground_add";
     }
 
--- src/main/java/football/start/allOfFootball/enums/LocationEnum.java
+++ src/main/java/football/start/allOfFootball/enums/LocationEnum.java
@@ -2,22 +2,32 @@ package football.start.allOfFootball.enums;
 
 public enum LocationEnum {
 
-    전체,
-    서울,
-    경기,
-    인천,
-    강원,
-    대전,
-    세종,
-    충남,
-    충북,
-    대구,
-    경북,
-    경남,
-    부산,
-    울산,
-    광주,
-    전북,
-    전남,
-    제주;
+    전체("regionAll"),
+    서울("seoul"),
+    경기("gyeonggi"),
+    인천("incheon"),
+    강원("gangwon"),
+    대전("daejeon"),
+    세종("sejong"),
+    충남("chungnam"),
+    충북("chungbuk"),
+    대구("daegu"),
+    경북("gyeongbuk"),
+    경남("gyeongnam"),
+    부산("busan"),
+    울산("ulsan"),
+    광주("gwangju"),
+    전북("jeonbuk"),
+    전남("jeonnam"),
+    제주("jeju");
+
+    private String regionId;
+
+    LocationEnum(String regionId) {
+        this.regionId = regionId;
+    }
+
+    public String getRegionId() {
+        return regionId;
+    }
 }
