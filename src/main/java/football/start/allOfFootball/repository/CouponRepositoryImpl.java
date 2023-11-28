99f3228ca40932ed08cfd58dfd2fdcaa34f7e7da
김승우 <tmd8633@naver.com>
2023-11-28 20:10:50 +0900

    Add : CouponRepositoryImpl DI 일부수정

--- src/main/java/football/start/allOfFootball/repository/CouponRepositoryImpl.java
+++ src/main/java/football/start/allOfFootball/repository/CouponRepositoryImpl.java
@@ -5,20 +5,24 @@ import football.start.allOfFootball.domain.Coupon;
 import football.start.allOfFootball.jpaRepository.JpaCouponListRepository;
 import football.start.allOfFootball.jpaRepository.JpaCouponRepository;
 import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Slf4j
-@RequiredArgsConstructor
+@Transactional
 public class CouponRepositoryImpl implements CouponRepository {
 
     private final JpaCouponRepository jpaCouponRepository;
     private final JpaCouponListRepository jpaCouponListRepository;
     private final JPAQueryFactory query;
 
-
+    public CouponRepositoryImpl(JpaCouponRepository jpaCouponRepository, JpaCouponListRepository jpaCouponListRepository, EntityManager em) {
+        this.jpaCouponRepository = jpaCouponRepository;
+        this.jpaCouponListRepository = jpaCouponListRepository;
+        this.query = new JPAQueryFactory(em);
+    }
 
     @Override
     public void createCoupon(Coupon coupon) {
@@ -28,9 +32,9 @@ public class CouponRepositoryImpl implements CouponRepository {
     @Override
     public void deleteCouponList(Long couponId) {
         // 쿠폰리스트에서 해당 쿠폰리스트를 모두 삭제 ( CASCADE 효과와 같음)
-        jpaCouponListRepository.deleteByCouponId(couponId);
+//        jpaCouponListRepository.deleteByCouponId(couponId);
 
         // 이후에 쿠폰 삭제
-        jpaCouponRepository.deleteById(couponId);
+//        jpaCouponRepository.deleteById(couponId);
     }
 }
