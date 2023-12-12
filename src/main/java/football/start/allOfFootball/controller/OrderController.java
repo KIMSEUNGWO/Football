d8c999454a70db7c8b3ba680a29a370b8d3e47ce
김승우 <tmd8633@naver.com>
2023-12-12 16:15:54 +0900

    Refactor : Order 성별, 등급 조건식 추가

--- src/main/java/football/start/allOfFootball/controller/OrderController.java
+++ src/main/java/football/start/allOfFootball/controller/OrderController.java
@@ -9,11 +9,15 @@ import football.start.allOfFootball.domain.Orders;
 import football.start.allOfFootball.dto.CouponListForm;
 import football.start.allOfFootball.dto.OrderForm;
 import football.start.allOfFootball.dto.OrderPostForm;
+import football.start.allOfFootball.enums.GenderEnum;
+import football.start.allOfFootball.enums.gradeEnums.MatchEnum;
 import football.start.allOfFootball.service.OrderService;
 import football.start.allOfFootball.service.domainService.CouponListService;
 import football.start.allOfFootball.service.domainService.MatchService;
 import football.start.allOfFootball.service.domainService.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -24,6 +28,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static football.start.allOfFootball.SessionConst.LOGIN_MEMBER;
+import static football.start.allOfFootball.SessionConst.REDIRECT_URL;
 
 @Controller
 @RequiredArgsConstructor
@@ -38,9 +43,15 @@ public class OrderController {
 
 
     @GetMapping("/order/{matchId}")
-    public String order(@PathVariable Long matchId, @SessionAttribute(name = LOGIN_MEMBER, required = false) Long memberId, Model model, HttpServletResponse response) {
-        if (matchId == null || memberId == null) {
+    public String order(@PathVariable Long matchId, @SessionAttribute(name = LOGIN_MEMBER, required = false) Long memberId, Model model, HttpServletResponse response, HttpServletRequest request) {
+        if (matchId == null) {
             return AlertUtils.alertAndBack(response, "비정상적인 접근입니다.");
+        }
+        if (memberId == null) {
+            String redirectURL = request.getHeader("referer");
+            HttpSession session = request.getSession();
+            session.setAttribute(REDIRECT_URL, redirectURL);
+            return AlertUtils.alertAndMove(response, "로그인이 필요합니다.", "/login");
         }
 
         Optional<Match> findMatch = matchService.findByMatch(matchId);
@@ -51,6 +62,13 @@ public class OrderController {
 
         Match match = findMatch.get();
         Member member = findMember.get();
+
+        if (match.getMatchGender() != GenderEnum.전체 && match.getMatchGender() != member.getMemberGender()) {
+            return AlertUtils.alert(response, match.getMatchGender() + "만 참여할 수 있습니다.");
+        }
+        if (match.getMatchGrade() != MatchEnum.전체 && !match.getMatchGrade().getGradeList().contains(member.getGrade())) {
+            return AlertUtils.alert(response, match.getMatchGrade().getMatchInfo() + "만 참여할 수 있습니다.");
+        }
         List<CouponListForm> couponList = couponListService.getCouponList(member);
         boolean distinctMember = matchService.distinctCheck(match, memberId);
         if (distinctMember) {
