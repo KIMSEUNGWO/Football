0f932cd8a77d80535a43b0433a2f7df1bda97503
김승우 <tmd8633@naver.com>
2023-12-12 14:46:51 +0900

    Refactor : GenderEnum JsonFormat 적용 -> 조회결과에서 성별에 따른 UI 변화 적용시 필요

--- src/main/java/football/start/allOfFootball/enums/GenderEnum.java
+++ src/main/java/football/start/allOfFootball/enums/GenderEnum.java
@@ -1,18 +1,19 @@
 package football.start.allOfFootball.enums;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum GenderEnum {
 
-    전체("남녀무관"),
-    남자("남자만"),
-    여자("여자만");
+    전체("남녀무관", "mix"),
+    남자("남자만", "male"),
+    여자("여자만", "female");
 
     public String matchInfo;
+    public String className;
 
-    GenderEnum(String matchInfo) {
-        this.matchInfo = matchInfo;
-    }
-
-    public String getMatchInfo() {
-        return matchInfo;
-    }
 }
