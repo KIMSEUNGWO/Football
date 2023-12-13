543131b5392a94c1849c057a63c3807ee83cd140
김승우 <tmd8633@naver.com>
2023-12-13 16:51:55 +0900

    Refactor : date 폼 추가

--- src/main/java/football/start/allOfFootball/formatter/DateFormatter.java
+++ src/main/java/football/start/allOfFootball/formatter/DateFormatter.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 public class DateFormatter {
 
     private static final String[] dayOfWeek = {"월요일", "화요일","수요일","목요일","금요일","토요일","일요일"};
+    private static final String[] dayOfWeek2 = {"월", "화","수","목","금","토","일"};
 
     public static String dateFormat(LocalDate date, int hourDate) {
         int year = date.getYear();
@@ -17,6 +18,14 @@ public class DateFormatter {
         String hour = String.format("%02d:00", hourDate);
 
         return String.format("%d월 %d일 %s %s", month, day, week, hour);
+    }
+    public static String dateFormat(LocalDate date) {
+        int year = date.getYear();
+        int month = date.getMonthValue();
+        int day = date.getDayOfMonth();
+        String week = dayOfWeek2[date.getDayOfWeek().getValue()-1];
+
+        return String.format("%d년 %d월 %d일 (%s)", year, month, day, week);
     }
 
     public static String sideMenuDateForm(LocalDate date) {
