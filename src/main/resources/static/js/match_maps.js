f89b0988ccd7956cd1d72dea05f7195537860dfb
김승우 <tmd8633@naver.com>
2023-12-14 04:13:21 +0900

    Add : 매치 네이버 지도 API 추가

---
+++ src/main/resources/static/js/match_maps.js
@@ -0,0 +1,36 @@
+가window.addEventListener('load', () => {
+    let mapsBtn = document.querySelector('.fieldAddress');
+    let maps = document.querySelector('#maps');
+    mapsBtn.addEventListener('click', () => {
+        maps.classList.toggle('disabled');
+        let mapBox = document.querySelector('#map');
+        if (mapBox.children.length == 0) {
+            showMaps(mapsBtn.textContent);
+        }
+    })
+})
+
+function showMaps(address) {
+    naver.maps.Service.geocode({ query: address }, function(status, response) {
+        if (status === naver.maps.Service.Status.ERROR) {
+            return alert('Something wrong!');
+        }
+
+        let result = response.v2; // 검색 결과의 컨테이너
+        let x = Number(result.addresses[0].x);
+        let y = Number(result.addresses[0].y);
+        var map = new naver.maps.Map('map', {
+            center: new naver.maps.LatLng(y, x),
+            zoom: 15
+        });
+        
+        var marker = new naver.maps.Marker({
+            position: new naver.maps.LatLng(y, x),
+            map: map
+        });
+        
+        console.log(response);
+        // 성공 시의 response 처리
+    });
+    
+}
