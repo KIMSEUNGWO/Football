5697c362e2c03ba922df22a8ab8048d8beb6724e
김승우 <tmd8633@naver.com>
2023-12-19 20:02:12 +0900

    Refactor : Null 처리

--- src/main/java/football/start/allOfFootball/service/domainService/CouponListService.java
+++ src/main/java/football/start/allOfFootball/service/domainService/CouponListService.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -21,6 +22,7 @@ public class CouponListService {
 
     public List<CouponListForm> getCouponList(Member member) {
         List<CouponList> couponList = couponListRepository.getCouponList(member);
+        if (couponList.isEmpty()) Collections.emptyList();
         return couponList.stream().map(x -> CouponListForm.build(x)).collect(Collectors.toList());
     }
 
