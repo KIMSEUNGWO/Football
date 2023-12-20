13ecd16aa5c8c2a216c17a6e486f06407265490d
김승우 <tmd8633@naver.com>
2023-12-20 14:24:30 +0900

    Add : 스케쥴에 랭킹 등록

--- src/main/java/football/start/allOfFootball/common/batch/OrderScheduled.java
+++ src/main/java/football/start/allOfFootball/common/batch/OrderScheduled.java
@@ -1,10 +1,13 @@
 package football.start.allOfFootball.common.batch;
 
 import football.start.allOfFootball.common.MatchTeamAlgorithms;
+import football.start.allOfFootball.common.redis.RankService;
 import football.start.allOfFootball.domain.Match;
+import football.start.allOfFootball.domain.Member;
 import football.start.allOfFootball.domain.Orders;
 import football.start.allOfFootball.enums.TeamEnum;
 import football.start.allOfFootball.enums.paymentEnums.RefundEnum;
+import football.start.allOfFootball.jpaRepository.JpaMemberRepository;
 import football.start.allOfFootball.service.OrderService;
 import football.start.allOfFootball.service.RefundService;
 import football.start.allOfFootball.service.domainService.MatchService;
@@ -28,6 +31,8 @@ public class OrderScheduled {
     private final MatchService matchService;
     private final OrderService orderService;
     private final RefundService refundService;
+    private final JpaMemberRepository jpaMemberRepository;
+    private final RankService rankService;
 
     // 경기 매치 전 최소인원 체크 및 알고리즘에 의한 팀 설정
     @Scheduled(cron = "0 30 0/2 * * ?") // 매일 경기시작 90분전에 경기마감처리 ( 4시 경기면 2시 30분에 스케쥴 시작 )
@@ -65,8 +70,13 @@ public class OrderScheduled {
     }
 
     // 결과 집계
-    @Scheduled(cron = "0 30 0/2 * * ?") // 2시간 간격으로 30분에 실행
+    @Scheduled(cron = "0 30 0/2 * * ?") // 2시간 간격으로 30분에 실행 ( 경기 종료 후 30분 후 랭킹최신화 )
     public void matchResult() {
-
+        List<Member> memberAll = jpaMemberRepository.findAll();
+        for (Member member : memberAll) {
+            Long memberId = member.getMemberId();
+            int score = member.getMemberScore();
+            rankService.updateRank(memberId, score);
+        }
     }
 }
