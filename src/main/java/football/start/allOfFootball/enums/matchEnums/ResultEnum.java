e6e5a02ec49fef201541f73f865a3b4250ec4cec
김승우 <tmd8633@naver.com>
2023-12-26 21:36:49 +0900

    Refactor : display, className Enum에 추가

--- src/main/java/football/start/allOfFootball/enums/matchEnums/ResultEnum.java
+++ src/main/java/football/start/allOfFootball/enums/matchEnums/ResultEnum.java
@@ -1,8 +1,16 @@
 package football.start.allOfFootball.enums.matchEnums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum ResultEnum {
 
-    승,
-    패,
-    무
+    승("WIN", "win"),
+    패("LOSW", "lose"),
+    무("DRAW", "draw");
+
+    private final String display;
+    private final String className;
 }
