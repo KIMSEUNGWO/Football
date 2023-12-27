4d5355de58749f8f7dde521979e28e661d905f3f
김승우 <tmd8633@naver.com>
2023-12-27 19:05:51 +0900

    Add : Social 도메인 추가

---
+++ src/main/java/football/start/allOfFootball/domain/Social.java
@@ -0,0 +1,34 @@
+package football.start.allOfFootball.domain;
+
+import football.start.allOfFootball.enums.SocialEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+@Builder
+@Getter
+@Entity
+@Table(name = "SOCIAL")
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamicInsert
+@SequenceGenerator(name = "SEQ_SOCIAL", sequenceName = "SEQ_SOCIAL_ID", allocationSize = 1)
+public class Social {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_SOCIAL")
+    private Long socialId;
+
+
+    @OneToOne
+    @JoinColumn(name = "memberId")
+    private Member member;
+
+    private Integer socialNum; // 고유번호
+
+    @Enumerated(EnumType.STRING)
+    private SocialEnum socialType;
+}
