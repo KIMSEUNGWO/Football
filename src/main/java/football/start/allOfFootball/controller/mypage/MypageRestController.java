483fcc953b73404e748fa8a6040750350a44c0fb
김승우 <tmd8633@naver.com>
2023-12-27 15:16:14 +0900

    Refactor : RestController 통합

--- src/main/java/football/start/allOfFootball/controller/mypage/MypageRestController.java
+++ src/main/java/football/start/allOfFootball/controller/mypage/MypageRestController.java
@@ -2,8 +2,10 @@ package football.start.allOfFootball.controller.mypage;
 
 import football.start.allOfFootball.SessionConst;
 import football.start.allOfFootball.domain.Member;
+import football.start.allOfFootball.domain.Orders;
 import football.start.allOfFootball.dto.ChangePasswordForm;
 import football.start.allOfFootball.service.MypageService;
+import football.start.allOfFootball.service.OrderService;
 import football.start.allOfFootball.service.domainService.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,6 +28,23 @@ public class MypageRestController {
 
     private final MypageService mypageService;
     private final MemberService memberService;
+    private final OrderService orderService;
+
+
+    @PostMapping("/mypage/order/get")
+    public List<OrderListForm> orderList(@SessionAttribute(name = LOGIN_MEMBER, required = false) Long memberId,
+                                         @RequestBody OrderDateForm form) {
+        System.out.println("orderList 시작");
+        Optional<Member> byMemberId = memberService.findByMemberId(memberId);
+        if (byMemberId.isEmpty()) return null;
+
+        Member member = byMemberId.get();
+        System.out.println("form = " + form);
+        List<Orders> orderList = orderService.findByMatchAll(member, form);
+        List<OrderListForm> matchResultForms = orderService.getMatchResultForm(orderList);
+
+        return matchResultForms;
+    }
 
     @PostMapping("/change/password")
     public Map<String, String> changePassword(@Validated @RequestBody ChangePasswordForm form, BindingResult bindingResult,
