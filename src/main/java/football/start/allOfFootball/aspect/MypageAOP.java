bee921047595b4c4a0491b030bea97c2e78e8014
김승우 <tmd8633@naver.com>
2023-12-28 19:42:01 +0900

    Refactor : 코드 간소화 ( AOP에서 Member 객체를 Model에 담고 컨트롤러 진입 )

--- src/main/java/football/start/allOfFootball/aspect/MypageAOP.java
+++ src/main/java/football/start/allOfFootball/aspect/MypageAOP.java
@@ -6,6 +6,7 @@ import football.start.allOfFootball.domain.Member;
 import football.start.allOfFootball.service.MypageService;
 import football.start.allOfFootball.service.domainService.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
@@ -13,11 +14,8 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-
-import java.util.Optional;
 
 @Aspect
 @Slf4j
@@ -33,20 +31,22 @@ public class MypageAOP {
 
         log.info("MyPageController Aspect Before 실행 : {}", joinPoint.getSignature().getName());
 
-        Object[] args = joinPoint.getArgs();
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        HttpSession session = request.getSession();
 
-        Long memberId = getMemberId(args);
-        Model model = getModel(args);
-        Optional<Member> byMemberId = memberService.findByMemberId(memberId);
-        if (byMemberId.isEmpty()) return;
+        Long memberId = (Long) session.getAttribute(SessionConst.LOGIN_MEMBER);
+        Model model = getModel(joinPoint.getArgs());
 
-        Member member = byMemberId.get();
+        Member member = memberService.findByMemberId(memberId).get();
         MyProfileDto myProfileDto = mypageService.getMyProfile(member);
+        String location = getMenu(request);
 
-        String location = getMypageLocation();
-
-        model.addAttribute("profile", myProfileDto);
-        model.addAttribute("menu", location);
+        if (model != null) {
+            model.addAttribute("member", member);
+            model.addAttribute("profile", myProfileDto);
+            model.addAttribute("menu", location);
+        }
     }
 
     private Model getModel(Object[] args) {
@@ -58,19 +58,7 @@ public class MypageAOP {
         return null;
     }
 
-    private Long getMemberId(Object[] args) {
-        for (Object arg : args) {
-            if (arg instanceof Long) {
-                return (Long) arg;
-            }
-        }
-        return null;
-    }
-
-    private String getMypageLocation() {
-        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        HttpServletRequest request = attributes.getRequest();
-
+    private String getMenu(HttpServletRequest request) {
         // 요청된 URL 가져오기
         String requestUrl = request.getRequestURL().toString();
         int lastIndex = requestUrl.lastIndexOf("/") + 1;
--- src/main/java/football/start/allOfFootball/controller/mypage/MypageController.java
+++ src/main/java/football/start/allOfFootball/controller/mypage/MypageController.java
@@ -4,7 +4,6 @@ import football.start.allOfFootball.domain.Member;
 import football.start.allOfFootball.dto.CouponListForm;
 import football.start.allOfFootball.service.MypageService;
 import football.start.allOfFootball.service.domainService.CouponListService;
-import football.start.allOfFootball.service.domainService.MatchService;
 import football.start.allOfFootball.service.domainService.MemberService;
 import football.start.allOfFootball.service.domainService.PaymentService;
 import lombok.RequiredArgsConstructor;
@@ -13,13 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-
-import static football.start.allOfFootball.SessionConst.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -30,66 +25,43 @@ public class MypageController {
     private final MypageService mypageService;
     private final CouponListService couponListService;
     private final PaymentService paymentService;
-    private final MemberService memberService;
 
     @GetMapping
-    public String mainPage(@SessionAttribute(name = LOGIN_MEMBER, required = false) Long memberId, Model model) {
-        Optional<Member> optionalMember = memberService.findByMemberId(memberId);
-        if (optionalMember.isEmpty()) {
-            log.info("사용자 정보가 없습니다");
-            return "redirect:/";
-        }
-        Member findMember = optionalMember.get();
-        MypageMainDto mypageMainDto = mypageService.getMypageMain(findMember);
+    public String mainPage(Model model) {
+        Member member = (Member) model.getAttribute("member");
+
+        MypageMainDto mypageMainDto = mypageService.getMypageMain(member);
         model.addAttribute("main", mypageMainDto);
         return "/mypage/mypage_main";
     }
 
     @GetMapping("/order")
-    public String orderList(@SessionAttribute(name = LOGIN_MEMBER, required = false) Long memberId, Model model) {
-        Optional<Member> optionalMember = memberService.findByMemberId(memberId);
-        if (optionalMember.isEmpty()) {
-            log.info("사용자 정보가 없습니다");
-            return "redirect:/";
-        }
+    public String orderList(Model model) {
+
         return "/mypage/mypage_order";
     }
     @GetMapping("/cash")
-    public String cashList(@SessionAttribute(name = LOGIN_MEMBER, required = false) Long memberId, Model model) {
-        Optional<Member> optionalMember = memberService.findByMemberId(memberId);
-        if (optionalMember.isEmpty()) {
-            log.info("사용자 정보가 없습니다");
-            return "redirect:/";
-        }
-        Member findMember = optionalMember.get();
+    public String cashList(Model model) {
+        Member member = (Member) model.getAttribute("member");
 
-        List<CashListForm> cashList = paymentService.findByAllMemberCashList(findMember);
+        List<CashListForm> cashList = paymentService.findByAllMemberCashList(member);
         model.addAttribute("cashList", cashList);
         return "/mypage/mypage_cash";
     }
     @GetMapping("/coupon")
-    public String couponList(@SessionAttribute(name = LOGIN_MEMBER, required = false) Long memberId, Model model) {
-        Optional<Member> optionalMember = memberService.findByMemberId(memberId);
-        if (optionalMember.isEmpty()) {
-            log.info("사용자 정보가 없습니다");
-            return "redirect:/";
-        }
-        Member findMember = optionalMember.get();
-        List<CouponListForm> couponList = couponListService.getCouponList(findMember);
+    public String couponList(Model model) {
+        Member member = (Member) model.getAttribute("member");
+
+        List<CouponListForm> couponList = couponListService.getCouponList(member);
         model.addAttribute("couponList", couponList);
         return "/mypage/mypage_coupon";
     }
 
     @GetMapping("/manager")
-    public String manager(@SessionAttribute(name = LOGIN_MEMBER, required = false) Long memberId, Model model) {
-        Optional<Member> optionalMember = memberService.findByMemberId(memberId);
-        if (optionalMember.isEmpty()) {
-            log.info("사용자 정보가 없습니다");
-            return "redirect:/";
-        }
-        Member findMember = optionalMember.get();
-        Map<String, List<ManagerDataForm>> list = mypageService.getManagerList(findMember);
+    public String manager(Model model) {
+        Member member = (Member) model.getAttribute("member");
 
+        Map<String, List<ManagerDataForm>> list = mypageService.getManagerList(member);
         model.addAttribute("matchList", list);
         return "/mypage/mypage_manager";
     }
