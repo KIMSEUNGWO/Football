657166d03a8ec95029cc40800447a5a8c8160452
김승우 <tmd8633@naver.com>
2024-01-20 19:23:46 +0900

    Refactor : memberService 제거

--- src/main/java/football/start/allOfFootball/controller/ManagerController.java
+++ src/main/java/football/start/allOfFootball/controller/ManagerController.java
@@ -1,35 +1,25 @@
 package football.start.allOfFootball.controller;
 
-import football.start.allOfFootball.SessionConst;
 import football.start.allOfFootball.customAnnotation.SessionLogin;
 import football.start.allOfFootball.domain.Member;
-import football.start.allOfFootball.dto.JsonDefault;
+import football.start.allOfFootball.dto.json.JsonDefault;
 import football.start.allOfFootball.dto.ManagerApplyDto;
-import football.start.allOfFootball.repository.domainRepository.ManagerRepository;
 import football.start.allOfFootball.service.domainService.ManagerService;
-import football.start.allOfFootball.service.domainService.MemberService;
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.TypeMismatchException;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 public class ManagerController {
 
-    private final MemberService memberService;
     private final ManagerService managerService;
 
 
