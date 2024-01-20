80ec87b2af631939654cc1c91a2bdaedab9ba29b
김승우 <tmd8633@naver.com>
2024-01-20 17:56:03 +0900

    Add : Json 객체화

---
+++ src/main/java/football/start/allOfFootball/dto/JsonDefault.java
@@ -0,0 +1,12 @@
+package football.start.allOfFootball.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class JsonDefault {
+
+    private String message;
+
+}
